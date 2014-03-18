e46e2482d15e73eb2f1de778f80f03b2166b756a
dinohadzic <dino.hadzic@wustl.edu>
2014-03-18 03:57:13 -0500

    retry
    retry

--- RegPack/R/RegPack-package.R
+++
@@ -1,8 +0,0 @@
-#' RegPack
-#'
-#' The RegPack package performs regressions on all possible covariates  
-#' @name RegPack
-#' @docType package
-#' @author  Dino Hadzic \email{dino.hadzic@@wustl.edu}
-#' 
-NULL
