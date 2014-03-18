95f00ebd1868d4ad50335f162da94abe62cf2776
dinohadzic <dino.hadzic@wustl.edu>
2014-03-18 03:59:40 -0500

    Revert "retry"
    This reverts commit e46e2482d15e73eb2f1de778f80f03b2166b756a.

---
+++ RegPack/R/RegPack-package.R
@@ -0,0 +1,8 @@
+#' RegPack
+#'
+#' The RegPack package performs regressions on all possible covariates  
+#' @name RegPack
+#' @docType package
+#' @author  Dino Hadzic \email{dino.hadzic@@wustl.edu}
+#' 
+NULL
